812b4fb7bf9f16e9ed095d6e0dc818f35528ff09
YarinGerbby <116896289+YarinGerbby@users.noreply.github.com>
2025-03-02 19:11:10 +0100

    Second Uplod

--- PotWeb/obj/Debug/net9.0/PotWeb.AssemblyInfo.cs
+++ PotWeb/obj/Debug/net9.0/PotWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PotWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce0860491d9c90743017d54ccd67cb2d1069124")]
 [assembly: System.Reflection.AssemblyProductAttribute("PotWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PotWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
